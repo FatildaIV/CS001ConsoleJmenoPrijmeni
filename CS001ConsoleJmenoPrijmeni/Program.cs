4d169615f5c2cf2e3cc9b9bec66f25c7275db23e
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 12:07:17 +0200

    Initial commit

---
+++ CS001ConsoleJmenoPrijmeni/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CS001ConsoleJmenoPrijmeni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj,");
+            Console.WriteLine("{0}!", JmenoPrijmeni());
+
+            Console.ReadKey();
+        }
+        static string JmenoPrijmeni()
+        {
+            return "Karel Hofman";
+        }
+    }
+}
